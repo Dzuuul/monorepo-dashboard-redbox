d363a7bf2a68dfea2902a8e3d1245d1cef84f3dc
Fikri <moh.dzulfikri.maulana@missiidea.com>
2025-07-21 10:57:49 +0700

    feat: add compability for windows

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: path.resolve(__dirname, "../.."), // pastikan absolute path
+  },
   /* config options here */
 };
 
